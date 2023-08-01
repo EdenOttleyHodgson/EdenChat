7044b15ded6168186f9527e7062de9dace43e0c0
EdenOttleyHodgson <lh12g22@soton.ac.uk>
2023-08-01 17:48:21 +0100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script lang='ts'>
+    import MainMenu from './components/MainMenu.vue'
+    export default {
+       components: {
+        MainMenu
+       } 
+    }
+
+</script>
+
+<template>
+    <MainMenu />
+</template>
+
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,13 @@
+<script lang = 'ts'> 
+
+</script>
+
+<template> 
+    <h1>Main Menu</h1>
+    <div>
+        <button>Begin Chatting</button>
+    </div>
+    <div>
+        <button>Settings</button>
+    </div>
+</template>
