c4e6b2f36475f442da34dd251b60f529999a8df3
EdenOttleyHodgson <lh12g22@soton.ac.uk>
2023-08-04 16:15:48 +0100

    styled message input area

--- src/components/MessageInputBox.vue
+++ src/components/MessageInputBox.vue
@@ -16,7 +16,19 @@
 
 <template> 
     <div class="box" :style="{height: '5%', marginTop: '10px'}">
-        <input v-model="currentMessage" placeholder="Enter Message Here"/>
-        <button @click="sendMessage">Send Message</button>
+        <input v-model="currentMessage" placeholder="Enter Message Here" class="messageInputArea"/>
+        <button @click="sendMessage" class="sendMessageButton">Send Message</button>
     </div>
 </template>
+<style> 
+    .messageInputArea {
+        width: 93%;
+        height: 90%;
+        float: left;
+        background: transparent;
+    }
+    .sendMessageButton{
+        height: 100%;
+        width: 6.1%
+    }
+</style>
